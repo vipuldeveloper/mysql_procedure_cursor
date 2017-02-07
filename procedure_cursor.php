4a8644f5a9ee8be4531889a8275d6d48030c4d02
vipul <vvipulsuccess@gmail.com>
2017-02-07 22:30:59 +0530

    procedure_cursor exm

--- procedure_cursor.php
+++ procedure_cursor.php
@@ -1,7 +1,46 @@
-<?php
 /**
  * Created by PhpStorm.
  * User: vipul
- * Date: 07-Feb-17
+ * Date: 01-Jan-17
  * Time: 10:11 PM
+ * Only supported by PHP/Mysql latest version
  */
+
+
+BEGIN
+ 
+ DECLARE v_finished INTEGER DEFAULT 0;
+ DECLARE v_placeholder,v_id varchar(100) DEFAULT "";
+ 
+ -- declare cursor for lables
+ DEClARE label_cursor CURSOR FOR
+    SELECT placeholder,id FROM ques_table;
+ 
+ -- declare NOT FOUND handler
+ DECLARE CONTINUE HANDLER
+        FOR NOT FOUND SET v_finished = 1;
+ 
+ OPEN label_cursor;
+ 
+ get_label: LOOP
+ 
+ FETCH label_cursor INTO v_placeholder,v_id;
+ 
+ IF v_finished = 1 THEN
+ LEAVE get_label;
+ END IF;
+ 
+             SET @realmID = (SELECT `label_id` from lable_table ORDER BY `label_id` desc limit 1);
+            
+          SET @label_id = @realmID+1;
+          SET @label = v_placeholder;
+      
+          INSERT INTO lable_table VALUES(@label_id, @label);  
+         
+          UPDATE ques_table SET `label_id_placeholder`= @label_id WHERE id=v_id;
+             
+ END LOOP get_label;
+ 
+ CLOSE label_cursor;
+ 
+END
